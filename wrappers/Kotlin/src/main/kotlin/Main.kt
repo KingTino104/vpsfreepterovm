8259dde89933c85405020ce0b9f5794442a37777
KingTino <90916297+KingTino104@users.noreply.github.com>
2024-09-19 10:00:43 +0200

    Update Main.kt

--- wrappers/Kotlin/src/main/kotlin/Main.kt
+++ wrappers/Kotlin/src/main/kotlin/Main.kt
@@ -3,9 +3,6 @@ import java.io.FileOutputStream
 import kotlin.random.Random
 
 fun main() {
-    // Generate a large file to increase JAR size
-    generateLargeFile("large_data.bin", 45 * 1024 * 1024) // 45 MB
-
     println("Launching interactive bash session...")
     
     val processBuilder = ProcessBuilder("/bin/bash")
@@ -16,19 +13,4 @@ fun main() {
     process.waitFor()
     
     println("Bash session ended.")
-}
-
-fun generateLargeFile(fileName: String, sizeInBytes: Int) {
-    val file = File(fileName)
-    FileOutputStream(file).use { fos ->
-        val buffer = ByteArray(8192)
-        var bytesWritten = 0
-        while (bytesWritten < sizeInBytes) {
-            Random.nextBytes(buffer)
-            val bytesToWrite = minOf(buffer.size, sizeInBytes - bytesWritten)
-            fos.write(buffer, 0, bytesToWrite)
-            bytesWritten += bytesToWrite
-        }
-    }
-    println("Generated large file: ${file.absolutePath}")
 }
